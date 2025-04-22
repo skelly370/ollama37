28bf2c2ea475ae2d53a47f7196fa53a0733d40de
skelly370 <seamus@keldo.uk>
2025-04-22 18:40:32 +0100

    Update gpu.go

--- discover/gpu.go
+++ discover/gpu.go
@@ -66,8 +66,8 @@ var (
 // With our current CUDA compile flags, older than 5.0 will not work properly
 // (string values used to allow ldflags overrides at build time)
 var (
-	CudaComputeMajorMin = "5"
-	CudaComputeMinorMin = "0"
+	CudaComputeMajorMin = "3"
+	CudaComputeMinorMin = "5"
 )
 
 var RocmComputeMajorMin = "9"
